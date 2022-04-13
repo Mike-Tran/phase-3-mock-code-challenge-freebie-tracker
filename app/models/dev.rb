e16636b86c16b4ec11b75a3a31a3828519f2a74d
Michael Tran <mltrunner@gmail.com>
2022-04-13 15:03:27 -0700

    Completed mock cc

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
-class Dev < ActiveRecord::Base
+require 'pry'
 
+class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        !!self.freebies.find_by(item_name: item_name)
+    end
+
+    def give_away(dev, freebie)
+        freebie.update(dev_id: dev.id) unless self.received_one?(freebie.item_name) && freebie.dev_id != self.id
+    end
 end
